3f3e6e25f2ffca10b483df20bec9a5cd28a77bb1
duypnt23 <duypnt23@gmail.com>
2021-07-21 22:22:21 +0700

    for master

--- Controller/Films.controller.js
+++ Controller/Films.controller.js
@@ -20,14 +20,7 @@ const getFilmDetail = async (req, res) => {
     } catch (err) {
         res.status(400).send(err);
     }
-    // const {id}=req.params;
-    // const film=filmList.find(film=>film.id==id);
-    // if(film){
-    //     res.send(film);
-    // }
-    // else{
-    //     res.status(404).send("Not Found");
-    // }
+    
 }
 const addNewFilm = async (req, res) => {
     const { title, year, director, actors, country, genre, description } = req.body;
@@ -37,20 +30,7 @@ const addNewFilm = async (req, res) => {
     } catch (err) {
         res.status(500).send(err);
     }
-    // console.log(req.body);
-    // const { title, year, director, actors, country, genre, description } = req.body;
-    // const film = {
-    //     id: filmList.length,
-    //     title,
-    //     year,
-    //     director,
-    //     actors,
-    //     country,
-    //     genre,
-    //     description
-    // }
-    // filmList.push(film);
-    // res.send(film);
+
 }
 const deleteFilms = async (req, res) => {
     const { id } = req.params;
@@ -59,14 +39,7 @@ const deleteFilms = async (req, res) => {
     } catch {
         res.status(404).send(err);
     }
-    // const film = filmList.find(film => film.id == id);
-    // if (film) {
-    //     filmList.splice(filmList.indexOf(film), 1);
-    //     res.send(film);
-    // }
-    // else {
-    //     res.status(404).send("Not Found");
-    // }
+
 }
 const updateFilm = async (req, res) => {
     const { id } = req.params;
@@ -77,19 +50,7 @@ const updateFilm = async (req, res) => {
 
     }
 
-    // const film = filmList.find(film => film.id == id);
-    // if (film) {
-    //     film.title = title;
-    //     film.year = year;
-    //     film.director = director;
-    //     film.actors = actors;
-    //     film.country = country;
-    //     film.genre = genre;
-    //     film.description = description;
-    //     res.send(film);
-    // }
-    // else {
-    //     res.status(404).send("Not Found");
-    // }
+ 
 }
+
 module.exports = { getFilmDetail, addNewFilm, deleteFilms, updateFilm, getFilmList };
