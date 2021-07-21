d03ed4811e17359f23921d73f1b43616ae020c49
duypnt23 <duypnt23@gmail.com>
2021-07-21 22:17:29 +0700

    for duy

--- Controller/Films.controller.js
+++ Controller/Films.controller.js
@@ -1,62 +1,95 @@
-const filmList=[];
+const { Films } = require("../models")
 
 
-const getFilmList=(req,res)=>{
-    res.send(filmList);
+
+
+
+const getFilmList = async (req, res) => {
+    try {
+        const Films = await Films.findAll({});
+        res.send(Films);
+    }
+    catch (err) {
+        res.status(500).send(err);
+    }
 }
-const getFilmDetail=(req,res)=>{
-    const {id}=req.params;
-    const film=filmList.find(film=>film.id==id);
-    if(film){
+const getFilmDetail = async (req, res) => {
+    try {
+        const film = await Films.findByPk(id);
         res.send(film);
+    } catch (err) {
+        res.status(400).send(err);
     }
-    else{
-        res.status(404).send("Not Found");
-    }
+    // const {id}=req.params;
+    // const film=filmList.find(film=>film.id==id);
+    // if(film){
+    //     res.send(film);
+    // }
+    // else{
+    //     res.status(404).send("Not Found");
+    // }
 }
-const addNewFilm=(req,res)=>{
-    console.log(req.body);
-    const {title,year,director,actors,country,genre,description}=req.body;
-    const film={
-        id:filmList.length,
-        title,
-        year,
-        director,
-        actors,
-        country,
-        genre,
-        description
+const addNewFilm = async (req, res) => {
+    const { title, year, director, actors, country, genre, description } = req.body;
+    try {
+        const film = await Films.create({ title, year, director, actors, country, genre, description });
+        res.status(201).send(film);
+    } catch (err) {
+        res.status(500).send(err);
     }
-    filmList.push(film);
-    res.send(film);
+    // console.log(req.body);
+    // const { title, year, director, actors, country, genre, description } = req.body;
+    // const film = {
+    //     id: filmList.length,
+    //     title,
+    //     year,
+    //     director,
+    //     actors,
+    //     country,
+    //     genre,
+    //     description
+    // }
+    // filmList.push(film);
+    // res.send(film);
 }
-const deleteFilms=(req,res)=>{
-    const {id}=req.params;
-    const film=filmList.find(film=>film.id==id);
-    if(film){
-        filmList.splice(filmList.indexOf(film),1);
-        res.send(film);
+const deleteFilms = async (req, res) => {
+    const { id } = req.params;
+    try {
+        await Films.destroy({ where: { id } });
+    } catch {
+        res.status(404).send(err);
     }
-    else{
-        res.status(404).send("Not Found");
-    }
+    // const film = filmList.find(film => film.id == id);
+    // if (film) {
+    //     filmList.splice(filmList.indexOf(film), 1);
+    //     res.send(film);
+    // }
+    // else {
+    //     res.status(404).send("Not Found");
+    // }
 }
-const updateFilm=(req,res)=>{
-    const {id}=req.params;
-    const {title,year,director,actors,country,genre,description}=req.body;
-    const film=filmList.find(film=>film.id==id);
-    if(film){
-        film.title=title;
-        film.year=year;
-        film.director=director;
-        film.actors=actors;
-        film.country=country;
-        film.genre=genre;
-        film.description=description;
-        res.send(film);
+const updateFilm = async (req, res) => {
+    const { id } = req.params;
+    const { title, year, director, actors, country, genre, description } = req.body;
+    try {
+        await Films.update({ title, year, director, actors, country, genre, description }, { where: { id } });
+    } catch {
+
     }
-    else{
-        res.status(404).send("Not Found");
-    }
+
+    // const film = filmList.find(film => film.id == id);
+    // if (film) {
+    //     film.title = title;
+    //     film.year = year;
+    //     film.director = director;
+    //     film.actors = actors;
+    //     film.country = country;
+    //     film.genre = genre;
+    //     film.description = description;
+    //     res.send(film);
+    // }
+    // else {
+    //     res.status(404).send("Not Found");
+    // }
 }
-module.exports={getFilmDetail,addNewFilm,deleteFilms,updateFilm,getFilmList};
+module.exports = { getFilmDetail, addNewFilm, deleteFilms, updateFilm, getFilmList };
---
+++ migrations/20210721095326-create-films.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Films', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      year: {
+        type: Sequelize.STRING
+      },
+      director: {
+        type: Sequelize.STRING
+      },
+      actors: {
+        type: Sequelize.STRING
+      },
+      country: {
+        type: Sequelize.STRING
+      },
+      genre: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Films');
+  }
+};
